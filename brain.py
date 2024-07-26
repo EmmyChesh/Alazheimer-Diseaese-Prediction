aaf63835a4411165045ba8393604d457e161313a
EmmyChesh <110698173+EmmyChesh@users.noreply.github.com>
2024-07-26 09:32:16 +0100

    brain.py

--- brain.py
+++ brain.py
@@ -3,7 +3,7 @@ import pandas as pd
 import pickle
 import catboost as cat
 
-model = pickle.load(open('Alzheimerr.sav, 'rb'))
+model = pickle.load(open('Alzheimerr.sav', 'rb'))
 
 # Initialize the input fields in Streamlit
 st.title('Health Data Prediction')
